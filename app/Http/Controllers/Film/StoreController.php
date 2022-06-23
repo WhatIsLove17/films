1d1ba9f0473a598a39e0bf358b3cddae47f2f348
WhatIsLove17 <WhatIsLoveff@gmail.com>
2022-06-23 15:29:38 +0300

    Requests were added

--- app/Http/Controllers/Film/StoreController.php
+++ app/Http/Controllers/Film/StoreController.php
@@ -3,18 +3,13 @@
 namespace App\Http\Controllers\Film;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Film\StoreRequest;
 use App\Models\Film;
 
 class StoreController extends Controller
 {
-    public function __invoke(){
-        $data = request()->validate([
-            'title'=>'required|string',
-            'description'=>'required|string',
-            'year'=>'required|date',
-            'category_id'=>'',
-            'tags'=>''
-        ]);
+    public function __invoke(StoreRequest $request){
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
         $film = Film::create($data);
--- app/Http/Controllers/Film/UpdateController.php
+++ app/Http/Controllers/Film/UpdateController.php
@@ -3,19 +3,14 @@
 namespace App\Http\Controllers\Film;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Film\UpdateRequest;
 use App\Models\Film;
 
 class UpdateController extends Controller
 {
 
-    public function __invoke(Film $film){
-        $data = request()->validate([
-            'title'=>'string',
-            'description'=>'string',
-            'year'=>'date',
-            'category_id'=>'',
-            'tags' => ''
-        ]);
+    public function __invoke(Film $film, UpdateRequest $request){
+        $data = $request->validated();
         $tags = $data['tags'];
         unset($data['tags']);
         $film->update($data);
